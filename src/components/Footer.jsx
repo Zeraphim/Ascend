3183c0fd3c68e5e3ebd22041c15eb62e0913a7af
Zeraphim <38757877+Zeraphim@users.noreply.github.com>
2024-04-06 00:25:56 +0800

    footer polishing, mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,15 @@ import ascendLogo from "../assets/icons/ascend_logo2_transparent.png"
 
 function Footer() {
   return (
-    <footer className="z-40 grid grid-cols-3 items-center absolute bottom-7 w-full" style={{ gridTemplateColumns: '47.5% 5% 47.5%' }}>
+    <footer className="z-40 grid grid-cols-3 items-center absolute bottom-7 w-full"
+        style={{
+            gridTemplateColumns: '47.5% 5% 47.5%',
+            '@media (max-width: 640px)': {
+            gridTemplateColumns: '40% 20% 40%',
+            }
+        }}>
         
-        <div className="text-[16px] font-light text-white text-right">
+        <div className="text-[16px] font-light text-white text-right lg:mr-0 mr-4">
             Ascend
         </div>
 
@@ -12,7 +18,7 @@ function Footer() {
             <img src={ascendLogo} alt="Ascend Logo" className="lg:w-10 lg:h-10 w-5 h-5"/>
         </div>
 
-        <div className="text-[16px] font-light text-white text-left">
+        <div className="text-[16px] font-light text-white text-left lg:ml-0 ml-4">
             by: Zeraphim
         </div>
 
